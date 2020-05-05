4c39c5e0cc7b278f312e71d584608aaf6f7786f3
Jake <36495719+JakeC007@users.noreply.github.com>
2020-05-05 18:31:16 -0400

    added writing to csv

--- generateAuthorCount.py
+++ generateAuthorCount.py
@@ -1,4 +1,5 @@
 import xlrd
+import csv
 wb = xlrd.open_workbook("hugo_data.xlsx") 
 
 #create dicts for 3 pronoun types found in data
@@ -18,6 +19,23 @@ for j in range(3):
       mDict[name]= mDict.setdefault(name, 0) + 1
     else:
       nbDict[name]= nbDict.setdefault(name, 0) + 1
+
+#write info to csv file
+with open('auth_stats.csv', mode='w') as auth_file:
+    auth_writer = csv.writer(auth_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    
+    auth_writer.writerow(['Name', 'Nominations', 'Pronouns', 'Freq']) #header row
+    
+    for entry in fDict.items():
+      auth_writer.writerow([entry[0], entry[1], 'She/Her', 1])
+    
+    for entry in mDict.items():
+      auth_writer.writerow([entry[0], entry[1], 'He/Him', 1])
+    
+    for entry in nbDict.items():
+      auth_writer.writerow([entry[0], entry[1], 'They/Them', 1])
+  
+
 
 #print sorted findings
 print("There are %d people with she/her pronouns" % len(fDict))
