eabb126c863b5472a93806a90873c31b0cbb5a55
JakeC007 <36495719+JakeC007@users.noreply.github.com>
2021-03-09 22:10:09 -0500

    refactor

---
+++ code/hugo_scrape.py
@@ -0,0 +1,243 @@
+"""
+Jake Chanenson
+3/9/2021
+Webscraper for Hugo Award Data 
+"""
+from bs4 import BeautifulSoup
+import urllib.request
+import os.path
+import csv
+from collections import defaultdict
+import time
+import re
+
+# Global Vars
+UNIQUE_AUTH = set()
+WHITELST = ["Best Novel","Best Novella", "Best Novelette", "Best Short Story"]
+
+def main():
+    
+  print("The 2020 Year")
+  foo = grabAwardYear(2020)
+  exportToCSV(foo, "test.csv", flag = True)
+  unqiueAuthToCSV("auth.csv")
+  #TODO
+  # fix regex issue 
+  # create rel path stuct for cache and csv file 
+  # fix the author CSV 
+  # - something is off with how many authors are added to the set and the whitelist..
+
+  # print("The 2019 Year")
+  # grabAwardYear(2019)
+
+
+def getpage(url, cache_file=None):
+
+    if cache_file is not None and os.path.exists(cache_file):
+        #print("Reading cache")
+        cache = open(cache_file, 'r')
+        page = cache.read()
+        return page
+
+    req = urllib.request.Request(url, headers={"User-Agent": "Chrome"})
+    res = urllib.request.urlopen(req)
+    page = res.read().decode('utf-8')
+
+    if cache_file is not None:
+        #print("Writing cache")
+        cache = open(cache_file, 'w')
+        cache.write(page)
+        cache.close()
+    
+    return page
+
+def grabAwardYear(year):
+  """
+  Grabs relevant content from Hugo Awrads Website
+  @param:
+    * year - int of which year to pull for hugo award data 
+  @returns:
+    * retDicts - list of dicts of all data for award year. Each element in lst is a different catagory
+  """
+  url = "http://www.thehugoawards.org/hugo-history/" + str(year) + "-hugo-awards/"
+  cFile = str(year)+'.html'
+  page = getpage(url,cache_file=cFile)
+  soup = BeautifulSoup(page)
+
+  nomData = [] # just the nominees
+  retDicts = [] # list of dicts of all data for award year  
+
+  results = soup.find_all("li", {"class":"winner"})
+  for winner in results:
+      category = winner.find_previous('strong')
+      print(f"** {category.text} **")
+      cat = category.text
+     
+      print(f"WINNER {winner.text}")
+      winData = winner.text
+      
+      all_nominees = winner.findPrevious().find_all('li')
+      
+      for i in range(1, len(all_nominees)):
+          nomData.append(all_nominees[i].text)
+          print(f"NOMINATED {all_nominees[i].text}") 
+      
+      bar = textProcess(year, cat, winData, nomData)
+
+      retDicts.append(bar)
+      nomData = []
+      print()
+
+  return retDicts
+
+
+def textProcess(year, cat, winData, nomData):
+  """
+  Take the scraped data of one catagory and pack it into a dict.
+  @params:
+    * year - int of the award year 
+    * cat - str of the award catagory
+    * winData - str of the winner of the catagory
+    * nomData - lst of strs of the nominees
+  @returns:
+    * TBD 
+  """
+  # set up varaibles 
+  tempDict = defaultdict(list)
+  pronouns = "U" # to be handled by hand at a later time
+    
+  # hanlde winner
+  win_bool = "TRUE"
+  title, author = cleanEntry(winData)
+  tempDict[title].extend([author, pronouns, cat, year, win_bool])
+
+  #hanlde nominees
+  win_bool = "FALSE"
+  for nominee in nomData:
+    title, author = cleanEntry(nominee)
+    tempDict[title].extend([author, pronouns, cat, year, win_bool])
+  return tempDict 
+
+def cleanEntry(text):
+  """
+  Use REGEX to grab the title and author from the nomination string
+  @param:
+    * test - the full text of the nomination string
+  @returns: 
+    * title - title of book string 
+    * author - author of book string
+  """
+  # set varaibles
+  title = ""
+  author = ""
+ 
+  try:
+    title = re.search('(.*), by (.*) \(', text).group(1)
+    author = re.search('(.*), by (.*) \(', text).group(2)
+  except:
+    try:
+      title = re.search('(.*), by (.*)', text).group(1)
+      author = title = re.search('(.*), by (.*)', text).group(2)
+    except AttributeError:
+      # print(f"\nThe string is {text}\n Author: {author}\n Title: {title}\n")
+      #TODO FIX REGEX THIS
+      pass
+  
+  UNIQUE_AUTH.add(author.strip('"“”')) #global var 
+  
+  return title.strip('"“"”'), author.strip('"“"”')
+
+def exportToCSV(dictLst, fileName, flag = False):
+  """
+	Appends a given dict to the CSV file
+	@params:
+		* awardDict - the dict containing all Hugo awards for a given year
+		* fileName - the name of the csv file 
+		* flag - optional, set to TRUE if you are creating the file 
+	@returns: None
+	"""
+  csvCol = ["Title", "Author", "Pronouns", "Category", "Year", "Winner_Bool"]
+  with open(fileName, 'a') as csvFP:
+    dictWriter = csv.writer(csvFP, delimiter=',')
+    
+    if flag == True:
+      dictWriter.writerow(csvCol)
+    
+    for d in dictLst:
+      print (list(d.values())[0][2])
+      if list(d.values())[0][2] not in WHITELST:
+        continue
+  
+      for title, data in d.items():
+        data.insert(0, title)
+        dictWriter.writerow(data)
+  
+  return None
+
+def unqiueAuthToCSV(auth_file):
+  """
+  Writes the unqiue set of authors to CSV. Needed to fill in pronouns by hand.
+  @params:
+    * auth_file - csv file
+  @returns:
+    * None 
+  """
+  #handle prexisiting file
+  if auth_file is not None and os.path.exists(auth_file):
+    authAppendHelper(auth_file)
+    return None
+  
+  #handle no file
+  if auth_file is not None:
+    with open(auth_file, 'a') as csvFP:
+      dictWriter = csv.writer(csvFP, delimiter=',')
+      dictWriter.writerow(["Author", "Pronouns"])
+      for author in UNIQUE_AUTH:
+        dictWriter.writerow([author, ""])
+  
+  return None
+
+def authAppendHelper(auth_file):
+  """
+  Helper function for unqiueAuthToCSV(). Reads in existing CSV and finds the difference between CSV author list and webscraped author list
+  @params:
+    * auth_file - csv file
+  @returns:
+    * None 
+  """
+  oldAuth = set()
+  # read in existing CSV and create set of existing authors
+  with open(auth_file, 'r') as csvFP:
+    csv_reader = csv.reader(csvFP, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            pass
+            line_count += 1
+        else:
+          oldAuth.add(row[0])
+          line_count += 1
+
+  #get the difference between sets
+  setDiff = UNIQUE_AUTH.difference(oldAuth)
+  print(f"Old {oldAuth}\n\n New: {UNIQUE_AUTH}")
+  print(setDiff)
+
+  # add new authors not already in set
+  with open(auth_file, 'a') as csvFP:
+    csv_writer = csv.writer(csvFP, delimiter=',')
+    for author in setDiff:
+      csv_writer.writerow([author, ""])
+  
+  return None
+
+
+
+
+
+
+
+main() 
+
+
+
